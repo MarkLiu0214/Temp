54f073d8220178a6e64d5eda2b4fba12b0b6b555
MarkLiu0214 <mark.liu0214@gmail.com>
2025-04-11 15:24:55 +0800

    Create g1_Move.cpp

---
+++ g1_Move.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <unitree/robot/g1/loco/g1_loco_api.hpp>
+#include <unitree/robot/g1/loco/g1_loco_client.hpp>
+#include <chrono>
+#include <thread>
+
+
+int  main(argc, char const *argv[]){
+    // 初始化通道工廠
+    unitree::robot::ChannelFactory::Instance()->Init(0, argv[1]);
+
+    // 初始化機器人移動客戶端
+    unitree::robot::g1::LocoClient locoClient;
+    locoClient.Init();
+    locoClient.SetTimeout(10.f);
+
+    /*
+    locoClient.Move(0.3f, 0, 0, true);
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    */
+
+    locoClient.Set_Velocity(0.3, 0, 0, 3);
+    locoClient.Set_Velocity(0, 0, -0.3, 3);
+    locoClient.Set_Velocity(0.3, 0, 0, 3);
+    locoClient.Set_Velocity(0, 0, 0.3, 3);
+
+
+
+}
